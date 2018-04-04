5863950a2fb5b467300d0ba4fd09e92a012ed0c4
gmiskell <georgia.miskell@gmail.com>
2018-04-04 14:17:26 +1200

    update

--- R/nearestsiteFUN.R
+++ R/nearestsiteFUN.R
@@ -7,41 +7,39 @@
 #' @export
 #' @examples
 #' nearestsiteFUN()
-  
+
 nearestsiteFUN <- function(x, obs, group, lat, lon){
 
   # install and load required packages
-  list.of.packages <- c("dplyr");
-  new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])];
-  if(length(new.packages)) install.packages(new.packages);
-  library(dplyr);
+  list.of.packages <- c("tidyverse")
+  new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+  if(length(new.packages)) install.packages(new.packages)
+  library(tidyverse)
 
   # define variables
-  x <- as.data.frame(x);
+  x <- as.data.frame(x)
   x$obs <- x[, obs]; x$group <- x[, group]; x$lat <- x[, lat]; x$lon <- x[, lon];
-  
+
   # find closest sites
   y <- x %>%
-    select(group, lat, lon) %>%
-    unique();
-	  
-  d <- as.matrix(dist(cbind(y$lon, y$lat)));
-  d <- data.frame(d);
-  colnames(d) <- c(as.character(d$group), 'group');	
-  min.d <- apply(d, 1, function(x) order(x, decreasing = F)[2]);
-  newdata <- cbind(y, y[min.d,], apply(d, 1, function(x) sort(x, decreasing=F)[2]));
-  colnames(newdata) <- c(colnames(y), 'n.group', 'n.lat', 'n.lon', 'distance');
-  newdata <- newdata[,c(1, 4:7)];
+    dplyr::select(group, lat, lon) %>%
+    unique()
+
+  d <- as.matrix(dist(cbind(y$lon, y$lat)))
+  d <- data.frame(d)
+  colnames(d) <- c(as.character(d$group), 'group')
+  min.d <- apply(d, 1, function(x) order(x, decreasing = F)[2])
+  newdata <- cbind(y, y[min.d,], apply(d, 1, function(x) sort(x, decreasing=F)[2]))
+  colnames(newdata) <- c(colnames(y), 'n.group', 'n.lat', 'n.lon', 'distance')
 
   # combine the data with appropriate proxy
-  df3 <- join(x, newdata, by = 'group');
-  x$nearest.proxy <- x$obs;
-  x$obs <- NULL;
-	
-  finaldata <- left_join(df3, x, by = c('date', 'n.group' = 'group'));
+  df3 <- full_join(x, newdata, by = 'group')
+  x$nearest.proxy <- x$obs
+  x$obs <- NULL
+
+  finaldata <- left_join(df3, x, by = c('date', 'n.group' = 'group'))
   finaldata <- finaldata %>%
-    select(date, group, obs, nearest.proxy);
-	
+    dplyr::select(date, group, obs, nearest.proxy)
 }
 
 
