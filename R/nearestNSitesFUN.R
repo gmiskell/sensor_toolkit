1d68653ec4bb1ecc841f7d5b3c2438e2b7ebc563
gmiskell <georgia.miskell@gmail.com>
2018-04-04 14:25:08 +1200

    add in two new functions

---
+++ R/nearestNSitesFUN.R
@@ -0,0 +1,45 @@
+#' Find the Nearest n Sites.
+#'
+#' A function for finding the closest n sites to a location.
+#' @param id Name of the location (as character string).
+#' @param lat Latitude column.
+#' @param lon Longitude column.
+#' @param n Number of nearest sites to find.
+#' @export
+#' @examples
+#' nearestNSitesFUN()
+
+
+nearestNSitesFUN <- function(x, id, lat, lon, n){
+  
+  library(stats);library(sp);library(rgeos);library(tidyverse)
+  output.file <- NULL
+  
+  x$id <- x[, id]; x$lat <- x[, lat]; x$lon <- x[, lon]
+  
+  x <- x %>%
+    dplyr::select(id, lat, lon) %>%
+    na.omit()
+  
+  sp.x = x
+  coordinates(sp.x) <- ~lon+lat
+  
+  d <- gDistance(sp.x, byid = T)
+  min.distance <- apply(d, 1, function(x) order(x, decreasing = F)[2:(n+1)])
+  
+  for(i in 1:n){
+    
+    distance.i <- x[min.distance[i,],]
+    combined.df <- cbind(x, distance.i)
+    combined.df$order <- i
+    colnames(combined.df) <- c(colnames(xx), "n_id", "n_lat", "n_lon", "order")
+    
+    if(exists('output.file')){
+      output.file <- rbind(output.file, combined.df)
+      output.file <- unique(output.file)
+    } else {
+      output.file <- combined.df
+    }
+  }
+  return(output.file)
+}
---
+++ R/sitesInNKMFUN.R
@@ -0,0 +1,51 @@
+#' Find All Sites Within n Radius.
+#'
+#' A function for finding all sites within a radius around a location.
+#' @param id Name of the location (as character string).
+#' @param lat Latitude column.
+#' @param lon Longitude column.
+#' @param n Number of nearest sites to find.
+#' @param as.list Option to unite the selected sites into a string (can be useful for later selection).
+#' @export
+#' @examples
+#' sitesInNKMFUN()
+
+sitesInNKMFUN <- function(x, id, lat, lon, n, as.list = F){
+  
+  library(stats);library(sp);library(rgeos);library(tidyverse)
+  
+  x$id <- x[, id]; x$lat <- x[, lat]; x$lon <- x[, lon]
+  
+  x <- x %>%
+    dplyr::select(id, lat, lon) %>%
+    na.omit() %>%
+    mutate(id = as.character(id))
+  
+  sp.x = x
+  coordinates(sp.x) <- ~lon+lat
+  
+  n.distance <- gWithinDistance(sp.x, byid = T, dist = n)
+  
+  x.dist <- cbind(x, n.distance)
+  colnames(x.dist) <- c(colnames(x), x$id)
+  
+  x.gather <- x.dist %>%
+    gather(n_id, in_n, -id, -lat, -lon) %>%
+    filter(id != n_id & in_n == TRUE) %>%
+    dplyr::select(-in_n) %>%
+    arrange(id)
+  
+  to.return <- x.gather
+  
+  if(as.list == TRUE){
+    
+    x.spread <- x.gather %>%
+      spread(n_id, n_id) %>%
+      mutate(n_id = apply(.[,-c(1:3)], 1, paste, collapse = "|")) %>%
+      dplyr::select(id, lat, lon, n_id)
+    
+    to.return <- x.spread
+  }
+  
+  return(to.return)
+}
