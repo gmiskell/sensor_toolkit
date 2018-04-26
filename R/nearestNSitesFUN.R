b5b0b82136917bbaaf983d5dd05c658aa349aedf
gmiskell <georgia.miskell@gmail.com>
2018-04-27 10:06:31 +1200

    updates

--- R/nearestNSitesFUN.R
+++ R/nearestNSitesFUN.R
@@ -10,30 +10,33 @@
 #' nearestNSitesFUN()
 
 
-nearestNSitesFUN <- function(x, id, lat, lon, n){
-  
-  library(stats);library(sp);library(rgeos);library(tidyverse)
+nearestNSitesFUN <- function(x, id, lat, lon, n, weighted = F){
+
+  library(stats);library(sp);library(geosphere);library(rgeos);library(tidyverse)
   output.file <- NULL
-  
+
   x$id <- x[, id]; x$lat <- x[, lat]; x$lon <- x[, lon]
-  
+
   x <- x %>%
     dplyr::select(id, lat, lon) %>%
     na.omit()
-  
+  rownames(x) <- seq(length = nrow(x))
+
   sp.x = x
   coordinates(sp.x) <- ~lon+lat
-  
+
   d <- gDistance(sp.x, byid = T)
-  min.distance <- apply(d, 1, function(x) order(x, decreasing = F)[2:(n+1)])
-  
+  min.distance <- apply(d, 2, function(x) order(x, decreasing = F)[2:(n+1)])
+
   for(i in 1:n){
-    
+
     distance.i <- x[min.distance[i,],]
     combined.df <- cbind(x, distance.i)
+    dist.min <- cbind(order = min.distance[i,], site = seq(length(min.distance[i,])))
+    combined.df$dist <- d[dist.min]
     combined.df$order <- i
-    colnames(combined.df) <- c(colnames(xx), "n_id", "n_lat", "n_lon", "order")
-    
+    colnames(combined.df) <- c(colnames(x), "n_id", "n_lat", "n_lon", "dist", "order")
+
     if(exists('output.file')){
       output.file <- rbind(output.file, combined.df)
       output.file <- unique(output.file)
